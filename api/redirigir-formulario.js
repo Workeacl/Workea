ea3a9d936067a026456a4f7d9cd614d97cc508f5
Workeacl <workea.cl@gmail.com>
2026-07-27 00:26:12 -0400

    Refactor payment ID retrieval using URL object

--- api/redirigir-formulario.js
+++ api/redirigir-formulario.js
@@ -3,7 +3,8 @@
 // la URL del formulario con el número de operación ya prellenado.
 
 export default async function handler(req, res) {
-  const paymentId = req.query.payment_id || req.query.collection_id || '';
+  const fullUrl = new URL(req.url, `https://${req.headers.host}`);
+  const paymentId = fullUrl.searchParams.get('payment_id') || fullUrl.searchParams.get('collection_id') || '';
 
   const FORM_BASE = 'https://docs.google.com/forms/d/e/1FAIpQLSe9UDo4wpX6gPw0rFjdl4_Jyr9gB5AUwckhuhOsoF5Gk_gv6g/viewform';
   const ENTRY_OPERACION = 'entry.1915985991';
